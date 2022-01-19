e4486203b31f8e9da80687de855e38f662ee39db
Hasnain-Raza-Gaho <gahohasnainraza@gmail.com>
2022-01-19 13:24:03 +0500

    map method

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+
+  const [obj,setobj] = useState([
+    {name:'Hasnain Raza', age : 26 , education: 'BS Hons' , id: 1},
+    {name:'Sajjad', age : 36 , education: 'B.A Commerce' , id: 2},
+    {name:'Sammer', age : 28 , education: 'Matriculation', id: 3},
+    {name:'Salman', age : 20 , education: 'Middle Pass' , id: 4},
+  ])
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+ <ul>
+   {obj.map(obj => (
+     <div key={obj.id}>
+     <li>{obj.name}</li>
+     <li>{obj.education}</li>
+     <br />
+     </div>
+     
+   ))}
+ </ul>
     </div>
   );
 }
