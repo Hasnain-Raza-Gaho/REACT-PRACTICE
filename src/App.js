0ed2e1f340c33fb87039522b179e0d99d3660044
Hasnain-Raza-Gaho <gahohasnainraza@gmail.com>
2022-01-19 21:33:18 +0500

    filter method and delete data from map method

--- src/App.js
+++ src/App.js
@@ -3,25 +3,44 @@ import React, { useState } from "react";
 function App() {
 
   const [obj,setobj] = useState([
-    {name:'Hasnain Raza', age : 26 , education: 'BS Hons' , id: 1},
-    {name:'Sajjad', age : 36 , education: 'B.A Commerce' , id: 2},
-    {name:'Sammer', age : 28 , education: 'Matriculation', id: 3},
-    {name:'Salman', age : 20 , education: 'Middle Pass' , id: 4},
+    {name:'Hasnain Raza', age : 26 , education: 'BS Hons' , id: 0},
+    {name:'Sajjad', age : 36 , education: 'B.A Commerce' , id: 1},
+    {name:'Sammer', age : 28 , education: 'Matriculation', id:2},
+    {name:'Salman', age : 20 , education: 'Middle Pass' , id: 3},
   ])
  
 
+const handleDelete = (id)=>{
+
+  const newdata = obj.filter(objs => objs.id !== id);
+
+  setobj(newdata)
+  console.log(newdata)
+}
+
   return (
+
     <div>
- <ul>
-   {obj.map(obj => (
-     <div key={obj.id}>
-     <li>{obj.name}</li>
-     <li>{obj.education}</li>
-     <br />
-     </div>
-     
-   ))}
- </ul>
+  {
+    obj.map((v,i)=>{
+return(
+  <div key={i}>
+  <div style={{backgroundColor : '#eeeeee'}}>
+<hr />
+        <div>Name: {v.name}</div>
+        <div>Education: {v.education}</div>
+        <div>Age: {v.age}</div>
+        <br />
+        <br />
+        <button onClick={()=>handleDelete(v.id)}>Delete</button>
+  </div>
+  <hr />
+  </div>
+        )
+      }
+    )
+  }
+ 
     </div>
   );
 }
