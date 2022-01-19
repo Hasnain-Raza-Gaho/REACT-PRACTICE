1540d56063268f5321e0935fb4086fe8841e82ec
Hasnain-Raza-Gaho <gahohasnainraza@gmail.com>
2022-01-19 22:36:58 +0500

    methods of useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
-
+console.log('this is first')
   const [obj,setobj] = useState([
     {name:'Hasnain Raza', age : 26 , education: 'BS Hons' , id: 0},
     {name:'Sajjad', age : 36 , education: 'B.A Commerce' , id: 1},
     {name:'Sammer', age : 28 , education: 'Matriculation', id:2},
     {name:'Salman', age : 20 , education: 'Middle Pass' , id: 3},
   ])
- 
+const [name,setname] = useState('Hasnain') 
 
 const handleDelete = (id)=>{
 
   const newdata = obj.filter(objs => objs.id !== id);
 
   setobj(newdata)
-  console.log(newdata)
 }
+
+
+useEffect(()=>{
+  console.log('useEffect running..only One.')
+},[])
+
+useEffect(()=>{
+  console.log('useEffect running for name...')
+},[name])
+
+
+console.log(obj)
+
 
   return (
 
     <div>
+<p>{name}</p>
+<button onClick={()=> setname('Hasnain Raza')}>Change Name</button>
+
+
+
   {
     obj.map((v,i)=>{
 return(
@@ -38,9 +55,16 @@ return(
   </div>
         )
       }
-    )
+      )
   }
  
+      
+        <button onClick={()=> setobj([
+          {name:'Hasnain Raza', age : 26 , education: 'BS Hons' , id: 0},
+          {name:'Sajjad', age : 36 , education: 'B.A Commerce' , id: 1},
+          {name:'Sammer', age : 28 , education: 'Matriculation', id:2},
+          {name:'Salman', age : 20 , education: 'Middle Pass' , id: 3},
+        ])}>Add All Data Again</button>
     </div>
   );
 }
