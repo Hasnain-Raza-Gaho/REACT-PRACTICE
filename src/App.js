4bcab220865dc1c21f50b79104e3c183c0c8f321
Hasnain-Raza-Gaho <gahohasnainraza@gmail.com>
2022-01-21 09:15:31 +0500

    loading effect when data is loading

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
+
+
 console.log('this is first')
+
+const [ispending,setpending]= useState(false)
+
   const [obj,setobj] = useState([
     {name:'Hasnain Raza', age : 26 , education: 'BS Hons' , id: 0},
     {name:'Sajjad', age : 36 , education: 'B.A Commerce' , id: 1},
     {name:'Sammer', age : 28 , education: 'Matriculation', id:2},
     {name:'Salman', age : 20 , education: 'Middle Pass' , id: 3},
   ])
-const [name,setname] = useState('Hasnain') 
+
 
 const handleDelete = (id)=>{
-
-  const newdata = obj.filter(objs => objs.id !== id);
+  setpending(true)
+setTimeout(() => {
+  
+const newdata = obj.filter(objs => objs.id !== id);
 
   setobj(newdata)
+  setpending(false)
+}, 1000);
 }
-
-
-useEffect(()=>{
-  console.log('useEffect running..only One.')
-},[])
-
-useEffect(()=>{
-  console.log('useEffect running for name...')
-},[name])
-
 
 console.log(obj)
 
@@ -33,15 +32,17 @@ console.log(obj)
   return (
 
     <div>
-<p>{name}</p>
-<button onClick={()=> setname('Hasnain Raza')}>Change Name</button>
 
 
 
+
+
+
+      {ispending && <h1>Loading...</h1> }
   {
     obj.map((v,i)=>{
-return(
-  <div key={i}>
+      return(
+        <div key={i}>
   <div style={{backgroundColor : '#eeeeee'}}>
 <hr />
         <div>Name: {v.name}</div>
